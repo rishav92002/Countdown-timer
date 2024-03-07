e5c8a1e68240f9d7bf0eef43cdb8d1a9c6d1f38a
Rishav Kumar <119895945+rishav92002@users.noreply.github.com>
2024-03-07 20:26:21 +0530

    modified script.js file to run for every year

--- script.js
+++ script.js
@@ -12,8 +12,11 @@ function countdown(){
 
     const newYearDate= new Date(2025,0,1);
     const curdate= new Date();
-
+    if(newYearDate.getFullYear()===curdate.getFullYear()){
+        newYearDate=new Date(newYearDate.getFullYear()+1,0,1)
+    }
     const seconds= (newYearDate-curdate)/1000;
+
     const days= Math.floor(seconds/(24*60*60));
     const hrs= Math.floor((seconds-(days*24*60*60))/3600);
     const mins= Math.floor((seconds%3600)/60);
